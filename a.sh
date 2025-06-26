0765234a59d49f61cb6accbbc3c925844f3f81bd
cipheror <cipherorcom@gmail.com>
2025-06-27 01:06:55 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,1 +1,1 @@
-NEZHA_SERVER=nezha.cipheror.com:443 NEZHA_KEY=IYrFLL8507qtcZ5ftVzv2KXbns8eTsp1 UUID=2a201112-9912-454c-9951-4a9d1753da1b bash <(curl -Ls https://main.ssss.nyc.mn/argox.sh)
+NEZHA_SERVER=nezha.cipheror.com:443 NEZHA_KEY=IYrFLL8507qtcZ5ftVzv2KXbns8eTsp1 UUID=1a255612-9932-453c-9941-4a9d1103da1b bash <(curl -Ls https://main.ssss.nyc.mn/argox.sh)
